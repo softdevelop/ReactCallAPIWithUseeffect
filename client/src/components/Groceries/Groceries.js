4dd9cfa656dcb201ba756a2773a9455cb5a3db64
Duong <linhduong.pscd@gmail.com>
2022-04-26 03:45:10 +0700

    Update filter & update data

--- client/src/components/Groceries/Groceries.js
+++ client/src/components/Groceries/Groceries.js
@@ -1,13 +1,12 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { getList, setItem, updateItem, deleteItem } from '../services/list';
+import { getList, setItem, deleteItem } from '../services/list';
 
 
 const Grocery = () => {
   const [alert, setAlert] = useState(false);
   const [itemInput, setItemInput] = useState('');
-  const [itemUpdate, setItemUpdate] = useState('');
+  //const [idInput, delIDInput] = useState('');
   const [itemFilter, setItemFilter] = useState('');
-  const [startUdate, setStartUdate] = useState(0);
   const [list, setList] = useState([]);
   const mounted = useRef(true);
 
@@ -40,33 +39,11 @@ const Grocery = () => {
     setItem(itemInput)
       .then(() => {
         if(mounted.current) {
-          setAlert(true);
-        }
-      })
-  };
-  
-  const handleUpdateItem = (e) => {
-    e.preventDefault();
-    updateItem(e.target.getAttribute('alt'), itemUpdate)
-      .then(() => {
-        if(mounted.current) {
           setItemInput('');
           setAlert(true);
         }
       })
   };
-  
-  const handleChangeItem = (e) => {
-  	setItemUpdate(e.target.value);
-  	let newList = [...list];
-  	newList.some(it => {
-  		if(it.id==e.target.alt) {
-  		 	it.item = e.target.value;
-  			return true;
-  		}
-  	});
-  	setList(newList);
-  }
   
   const delItem = (idInput) => {
     deleteItem(idInput)
@@ -80,8 +57,8 @@ const Grocery = () => {
   
   const filterData = (e) => {
     e.preventDefault();
-    //let fd = list.filter(dt => dt.item.includes(itemFilter));
-    //setItemFilter('');
+    let fd = list.filter(dt => dt.item.includes(itemFilter));
+    setItemFilter('');
   }
 
   return(
@@ -90,25 +67,10 @@ const Grocery = () => {
 		<div className="pt-0 shadow rounded-3 overflow-hidden d-flex list-group" id="dropdownFilter">
 			<div className="d-grid gap-1">
 				<form className="p-2 mb-2 bg-light border-bottom" onSubmit={filterData}>
-				  <input type="search" className="form-control" placeholder="Type to filter..." onChange={event => setItemFilter(event.target.value)}/>
+				  <input type="search" className="form-control" autoComplete="false" placeholder="Type to filter..." onChange={event => setItemFilter(event.target.value)}/>
 				</form>
 				<ul className="list-unstyled mb-0">
-				  { 
-				  	list
-						.filter(dt => dt.item.includes(itemFilter))
-				  		.map(item => 
-				  			<li className="list-group-item dropdown-item d-flex justify-content-between" key={item.item}>
-				  				{ startUdate? 
-				  					<form onSubmit={handleUpdateItem} alt={item.id}>
-				  						<input type="text" className="form-control" value={item.item} alt={item.id}  onChange={handleChangeItem}/>
-				  					</form> :
-				  					item.item
-				  				}
-				  				<button type="button" className="btn btn-warning" onClick={() => setStartUdate(!startUdate)}>Update</button>
-				  				<button type="button" className="btn-outline-danger btn-close" onClick={() => delItem(item.id)}></button>
-				  			</li>
-				  		)
-				  }
+				  {list.map(item => <li className="list-group-item dropdown-item d-flex align-items-center gap-2 py-2 justify-content-between align-items-start" key={item.item}>{item.item} <button type="button" className="btn-outline-danger rounded-pill btn-close" aria-label="Close" alt={item.id} onClick={() => delItem(item.id)}></button></li>)}
 				</ul>
 				{alert && <h2> Submit Successful</h2>}
 				<hr className="featurette-divider"/>
--- client/src/services/list.js
+++ client/src/services/list.js
@@ -1,11 +1,35 @@
+const domain = 'http://localhost:3333/';
+
 export function getList() {
-  return fetch('http://localhost:3333/list')
+  return fetch(domain+'list')
     .then(data => data.json())
 }
 
 export function setItem(item) {
- return fetch('http://localhost:3333/list', {
+ return fetch(domain+'list', {
    method: 'POST',
+   headers: {
+     'Content-Type': 'application/json'
+   },
+   body: JSON.stringify({ item })
+ })
+   .then(data => data.json())
+}
+
+export function deleteItem(id) {
+ return fetch(domain+'list/'+id, {
+   method: 'DELETE',
+   headers: {
+     'Content-Type': 'application/json'
+   }
+ })
+   .then(data => data.json())
+}
+
+export function updateItem(id, item) {
+ return fetch(domain+'list/'+id, {
+   method: 'PUT',
+   //method: 'PATCH',
    headers: {
      'Content-Type': 'application/json'
    },
